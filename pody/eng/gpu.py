1abd928326857c91d70120b6654c2911df6e34a4
limengxun <limengxun45@outlook.com>
2025-02-09 21:33:07 +0800

    more process info

--- pody/eng/gpu.py
+++ pody/eng/gpu.py
@@ -6,11 +6,6 @@ from .errors import InvalidInputError
 class GPUProcess:
     pid: int
     gpu_memory_used: int
-    cgroup: str
-
-def _cgroup_from_pid(pid: int) -> str:
-    with open(f"/proc/{pid}/cgroup") as f:
-        return f.read().split("\n")[0]
 
 def list_processes_on_gpus(gpu_ids: list[int]) -> dict[int, list[GPUProcess]]:
     """
@@ -31,12 +26,10 @@ def list_processes_on_gpus(gpu_ids: list[int]) -> dict[int, list[GPUProcess]]:
             GPUProcess(
                 pid=proc.pid,
                 gpu_memory_used=proc.usedGpuMemory,
-                cgroup=_cgroup_from_pid(proc.pid)
             ) for proc in info
         ]
     pynvml.nvmlShutdown()
     return processes
 
 if __name__ == "__main__":
-    print(_cgroup_from_pid(1))
     print(list_processes_on_gpus([0, 1]))
--- pody/svc/router_resource.py
+++ pody/svc/router_resource.py
@@ -7,6 +7,7 @@ from docker import DockerClient
 from ..eng.errors import *
 from ..eng.docker import query_container_by_id
 from ..eng.gpu import list_processes_on_gpus, GPUProcess
+from ..eng.cpu import query_process
 
 router_resource = APIRouter(prefix="/resource")
 
@@ -17,16 +18,16 @@ def gpu_status_impl(client: DockerClient, gpu_ids: list[int]):
         if not last.endswith(".scope"): return None
         return last[len("docker-"):-len(".scope")]
     def fmt_gpu_proc(gpu_proc: GPUProcess):
-        container_id = container_id_from_cgroup(gpu_proc.cgroup)
-        if container_id is None: return {"pid": gpu_proc.pid, "gpu_memory_used": gpu_proc.gpu_memory_used, "container": None}   # not running in a container
-        container_info = query_container_by_id(client, container_id)
-        if container_info is not None:
-            del container_info["ports"]
-            del container_info["status"]
+        process_info = query_process(gpu_proc.pid)
+        container_id = container_id_from_cgroup(process_info.cgroup)
+        container_name = query_container_by_id(client, container_id)["name"] if container_id else ""
         return {
             "pid": gpu_proc.pid,
+            "pod": container_name,
+            "cmd": process_info.cmd,
+            "uptime": process_info.uptime,
+            "memory_used": process_info.memory_used,
             "gpu_memory_used": gpu_proc.gpu_memory_used,
-            "container": container_info
         }
     gpu_procs = list_processes_on_gpus(gpu_ids)
     return {gpu_id: [fmt_gpu_proc(proc) for proc in gpu_procs[gpu_id]] for gpu_id in gpu_procs}
