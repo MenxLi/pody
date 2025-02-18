faef8460dc33ad0dcfadf2e3707d4dd0cf400395
limengxun <limengxun45@outlook.com>
2025-02-18 22:17:05 +0800

    fix: update version history for 0.1.10

--- pody/version.py
+++ pody/version.py
@@ -2,7 +2,10 @@
 VERSION_HISTORY = {
     "0.1.9": [
         "Improve help route, format help output as table", 
+    ], 
+    "0.1.10": [
+        "Remove /status route, add /host/spec and /version routes", 
     ]
 }
 
-VERSION = tuple([int(x) for x in list(VERSION_HISTORY.keys())[0].split('.')])
+VERSION = tuple([int(x) for x in list(VERSION_HISTORY.keys())[-1].split('.')])
